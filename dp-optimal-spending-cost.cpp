1948874428a1c58dad983e6c04577f09862080da
Sajal Shrestha <sajal@Sajals-MBP.lan>
2023-11-21 21:38:24 -0800

    1d: print out optimal solutions in each subsolutions

--- dp-optimal-spending-cost.cpp
+++ dp-optimal-spending-cost.cpp
@@ -16,13 +16,12 @@ vector<int> B = {2500, 1000, 3500, 2000}; // City B salaries
 
 int n = A.size(); // Size of the vector array
 
-// Recursive function to compute the optimal spending cost and store choices
-int OPT(int week, int city, vector<vector<int>> &memo, vector<vector<int>> &choices)
+// Recursive function to compute the optimal spending cost
+int OPT(int week, int city, const vector<int> &A, const vector<int> &B, vector<vector<int>> &memo, int F, int Da, int Db)
 {
     // Base case
     if (week == 1)
     {
-        choices[week][city] = city;
         return (city == 0) ? A[0] : B[0];
     }
 
@@ -35,18 +34,8 @@ int OPT(int week, int city, vector<vector<int>> &memo, vector<vector<int>> &choi
     int diffCity = (city == 0) ? 1 : 0;
 
     // Recursive cases
-    int sameCityCost = (city == 0) ? (A[week - 1] + OPT(week - 1, city, memo, choices) - Da) : (B[week - 1] + OPT(week - 1, city, memo, choices) - Db);
-    int diffCityCost = (city == 0) ? (A[week - 1] + OPT(week - 1, diffCity, memo, choices) + F) : (B[week - 1] + OPT(week - 1, diffCity, memo, choices) + F);
-
-    // Store choices for reconstruction
-    if (sameCityCost < diffCityCost)
-    {
-        choices[week][city] = city;
-    }
-    else
-    {
-        choices[week][city] = diffCity;
-    }
+    int sameCityCost = (city == 0) ? (A[week - 1] + OPT(week - 1, city, A, B, memo, F, Da, Db) - Da) : (B[week - 1] + OPT(week - 1, city, A, B, memo, F, Da, Db) - Db);
+    int diffCityCost = (city == 0) ? (A[week - 1] + OPT(week - 1, diffCity, A, B, memo, F, Da, Db) + F) : (B[week - 1] + OPT(week - 1, diffCity, A, B, memo, F, Da, Db) + F);
 
     // Memoize the result
     memo[week][city] = min(sameCityCost, diffCityCost);
@@ -54,34 +43,57 @@ int OPT(int week, int city, vector<vector<int>> &memo, vector<vector<int>> &choi
     return memo[week][city];
 }
 
+// Function to print the optimal solution
+void printOPTs(int week, int city, const vector<int> &A, const vector<int> &B, vector<vector<int>> &memo, int F, int Da, int Db)
+{
+    if (week == 1)
+    {
+        cout << "Week 1: City " << (city == 0 ? 'A' : 'B') << endl;
+        return;
+    }
+
+    int diffCity = (city == 0) ? 1 : 0;
+    int sameCityCost = (city == 0) ? (A[week - 1] + memo[week - 1][city] - Da) : (B[week - 1] + memo[week - 1][city] - Db);
+    int diffCityCost = (city == 0) ? (A[week - 1] + memo[week - 1][diffCity] + F) : (B[week - 1] + memo[week - 1][diffCity] + F);
+
+    if (memo[week][city] == sameCityCost)
+    {
+        printOPTs(week - 1, city, A, B, memo, F, Da, Db);
+        cout << "Week " << week << ": City " << (city == 0 ? 'A' : 'B') << endl;
+    }
+    else
+    {
+        printOPTs(week - 1, diffCity, A, B, memo, F, Da, Db);
+        cout << "Week " << week << ": City " << (diffCity == 0 ? 'A' : 'B') << endl;
+    }
+}
+
 int main()
 {
     // Memoization table
     vector<vector<int>> memo(n + 1, vector<int>(2, -1));
 
-    // Table to store choices for optimal solution reconstruction
-    vector<vector<int>> choices(n + 1, vector<int>(2, -1));
-
     // Find the optimal spending cost
-    int min_cost = min(OPT(n, 0, memo, choices), OPT(n, 1, memo, choices));
+    int min_cost = min(OPT(n, 0, A, B, memo, F, Da, Db), OPT(n, 1, A, B, memo, F, Da, Db));
+
+    // Print the optimal solution
+    cout << "Optimal Delivery Plan:" << endl;
+    printOPTs(n, (min_cost == memo[n][0]) ? 0 : 1, A, B, memo, F, Da, Db);
 
     // Write results to output file
-    ofstream outFile("spendcost_pc_out.txt");
+    ofstream outFile("spendcost_pd_out.txt");
     if (!outFile.is_open())
     {
         cerr << "Unable to open output file." << endl;
         return 1;
     }
 
-    outFile << "Table with Optimal Values for Subproblems and Choices:" << endl;
-    cout << "Table with Optimal Values for Subproblems and Choices:" << endl;
-
+    outFile << "Table with Optimal Values for Subproblems:" << endl;
+    cout << "Table with Optimal Values for Subproblems:" << endl;
     for (int i = 1; i <= n; ++i)
     {
-        string cho0 = choices[i][0] == 0 ? "A" : "B";
-        string cho1 = choices[i][1] == 0 ? "A" : "B";
-        outFile << "Week " << i << ": City A - $" << OPT(i, 0, memo, choices) << " (Choice: " << cho0 << "), City B - $" << OPT(i, 1, memo, choices) << " (Choice: " << cho1 << ")" << endl;
-        cout << "Week " << i << ": City A - $" << OPT(i, 0, memo, choices) << " (Choice: " << cho0 << "), City B - $" << OPT(i, 1, memo, choices) << " (Choice: " << cho1 << ")" << endl;
+        outFile << "Week " << i << ": City A - $" << OPT(i, 0, A, B, memo, F, Da, Db) << ", City B - $" << OPT(i, 1, A, B, memo, F, Da, Db) << endl;
+        cout << "Week " << i << ": City A - $" << OPT(i, 0, A, B, memo, F, Da, Db) << ", City B - $" << OPT(i, 1, A, B, memo, F, Da, Db) << endl;
     }
 
     outFile << endl
